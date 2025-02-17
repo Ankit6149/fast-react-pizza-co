6f766e776e1007e488256e3ca86612e23a900e3f
Ankit Bhardwaj <97785108+Ankit6149@users.noreply.github.com>
2025-02-18 00:49:25 +0530

    Update Username.jsx

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -5,14 +5,14 @@ function Username() {
 
   if (!username) return null;
   return (
-    <div className="flex items-center justify-center gap-2">
+    <div className="hidden flex items-center justify-center gap-2">
       <img
         height="24"
         width="24"
         src="https://img.icons8.com/fluency-systems-filled/96/user.png"
         alt="user"
       />
-      <div className="hidden text-sm font-semibold md:block">{username}</div>
+      <div className="text-sm font-semibold md:block">{username}</div>
     </div>
   );
 }
