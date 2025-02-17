23a08db85e3c733098422e35dd08871f1a65710a
Ankit Bhardwaj <97785108+Ankit6149@users.noreply.github.com>
2025-02-18 01:24:17 +0530

    improved design

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -9,6 +9,21 @@ function CartItem({ item }) {
 
   const currentQuantity = useSelector(getCurrentQuantityByID(pizzaId));
 
+  // return (
+  //   <li className="py-3 sm:flex sm:items-center sm:justify-between">
+  //     <p className="mb-1 sm:mb-0">
+  //       {quantity}&times; {name}
+  //     </p>
+  //     <div className="flex items-center justify-between sm:gap-6">
+  //       <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+  //       <UpdateItemQuantity
+  //         pizzaId={pizzaId}
+  //         currentQuantity={currentQuantity}
+  //       />
+  //       <DeleteItem pizzaId={pizzaId} />
+  //     </div>
+  //   </li>
+  // );
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
@@ -16,11 +31,13 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <UpdateItemQuantity
-          pizzaId={pizzaId}
-          currentQuantity={currentQuantity}
-        />
-        <DeleteItem pizzaId={pizzaId} />
+        <div className="flex gap-4 sm:gap-6">
+          <UpdateItemQuantity
+            pizzaId={pizzaId}
+            currentQuantity={currentQuantity}
+          />
+          <DeleteItem pizzaId={pizzaId} />
+        </div>
       </div>
     </li>
   );
